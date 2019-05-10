13a7d214e64b09d78898236edc13e1eb1e854a30
Martin-Vaida <goodluckfanweida@163.com>
2019-05-10 12:40:59 +0800

    1.0.3

--- test/StoryViewControlloers/MarryHadALittleLambController.swift
+++ test/StoryViewControlloers/MarryHadALittleLambController.swift
@@ -15,10 +15,38 @@ class MarryHadALittleLambController:UITableViewController {
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var labelTwo: UILabel!
     @IBOutlet weak var labelThree: UILabel!
+    @IBOutlet weak var labelFour: UILabel!
+    @IBOutlet weak var labelFive: UILabel!
+    @IBOutlet weak var labelSix: UILabel!
+    @IBOutlet weak var labelSeven: UILabel!
+    @IBOutlet weak var labelEight: UILabel!
+    @IBOutlet weak var labelNine: UILabel!
+    @IBOutlet weak var labelTen: UILabel!
+    @IBOutlet weak var labelEleven: UILabel!
+    @IBOutlet weak var labelTwelve: UILabel!
+    @IBOutlet weak var labelThirteen: UILabel!
+    @IBOutlet weak var labelFourteen: UILabel!
+    @IBOutlet weak var labelFifteen: UILabel!
     
     @IBOutlet weak var lineOne: UITextField!
     @IBOutlet weak var lineTow: UITextField!
     @IBOutlet weak var lineThree: UITextField!
+    @IBOutlet weak var lineFour: UITextField!
+    @IBOutlet weak var lineFive: UITextField!
+    @IBOutlet weak var lineSix: UITextField!
+    @IBOutlet weak var lineSeven: UITextField!
+    @IBOutlet weak var lineEight: UITextField!
+    @IBOutlet weak var lineNine: UITextField!
+    @IBOutlet weak var lineTen: UITextField!
+    @IBOutlet weak var lineEleven: UITextField!
+    @IBOutlet weak var lineTwelve: UITextField!
+    @IBOutlet weak var lineThirteen: UITextField!
+    @IBOutlet weak var lineFourteen: UITextField!
+    @IBOutlet weak var lineFifteen: UITextField!
+    
+    
+    //define texts
+    
     
     
     @IBAction func lineOneEnter(_ sender: Any) {
