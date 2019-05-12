c23ea858635ced4f92f3b7a6b4f18bcfdfd34bcd
Vaida <goodluckfanweida@163.com>
2019-05-12 19:22:12 +0800

    1.0.16 Updated UI, All lines can be used now!

--- test/StoryViewControlloers/MarryHadALittleLambController.swift
+++ test/StoryViewControlloers/MarryHadALittleLambController.swift
@@ -18,15 +18,13 @@ class MarryHadALittleLambController:UITableViewController {
     var accuracyTappedBool = false
     
     var tappedLetersInAll = -1
-    var lineOneTappedLeters = 0
-    
     var correctLettersInAll = 0
-    var lineOneCorrectLetters = 0
-    
-    let accuracy = 100
     
     var labelArray:[UILabel] = []
     var lineArray:[UITextField] = []
+    
+    var correctLettersArray = [Int](repeatElement(0, count: 15))
+    var tappedLettersArray = [Int](repeatElement(0, count: 15))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,7 +71,7 @@ class MarryHadALittleLambController:UITableViewController {
     
     //define texts
     func setup() {
-        labelOne.text = "Mary had a little lamb, little lamb, little lamb,"
+        labelOne.text = "Mary had a little lamb, little lamb, little lamb,"
         labelTwo.text = "Mary had a little lamb, its fleece was white as snow."
         labelThree.text = "And everywhere that Mary went, Mary went, Mary went,"
         labelFour.text = "And everywhere that Mary went, the lamb was sure to go."
@@ -196,10 +194,10 @@ class MarryHadALittleLambController:UITableViewController {
     
     func updateAccuracyLabel() {
         if accuracyTappedBool {
-            self.navigationItem.title = "Correct Letters: \(correctLettersInAll), Tapped Letters: \(tappedLetersInAll), Accuracy Rate: \(correctLettersInAll*accuracy/tappedLetersInAll)%"
+            self.navigationItem.title = "Correct Letters: \(correctLettersInAll), Tapped Letters: \(tappedLetersInAll), Accuracy Rate: \(correctLettersInAll*100/tappedLetersInAll)%"
             self.accuracylabel.title = "Accuracy Rate"
         } else {
-            self.accuracylabel.title = "Accuracy: \(correctLettersInAll*accuracy/tappedLetersInAll)%"
+            self.accuracylabel.title = "Accuracy: \(correctLettersInAll*100/tappedLetersInAll)%"
         }
     }
     
@@ -214,120 +212,134 @@ class MarryHadALittleLambController:UITableViewController {
     //When User changed Valve...
     func updateValue(line:Int) {
         
-        
-        if lineArray[line-1].text?.count == lineArray[line-1].text!.count+1 {
+        if lineArray[line-1].text?.count == labelArray[line-1].text!.count+1 {
+            guard line != 15 else {
+                stopTimer()
+                return
+            }
+            
             lineArray[line-1].resignFirstResponder()
             lineArray[line].text = " "
             lineArray[line].becomeFirstResponder()
             
             labelArray[line-1].textColor = .gray
             labelArray[line].textColor = .darkGray
+            
+            lineArray[line-1].textColor = .gray
+            lineArray[line].textColor = .black
         }
         
         guard lineArray[line-1].text != nil else { return }
         
         if lineArray[line-1].text!.count == 0 {
-            lineArray[line-1].text = " "
+            if line == 1 {
+                lineArray[line-1].text = " "
+            } else {
+                lineArray[line-1].resignFirstResponder()
+                lineArray[line-2].becomeFirstResponder()
+                
+                labelArray[line-1].textColor = .gray
+                labelArray[line-2].textColor = .darkGray
+                
+                lineArray[line-1].textColor = .gray
+                lineArray[line-2].textColor = .black
+            }
         }
         
         guard labelArray[line-1].text != nil else { return }
-        guard lineOne.text!.count >= 2 else { return }
-        tappedLetersInAll = lineOne.text!.count
+        guard lineArray[line-1].text!.count >= 2 else { return }
+        tappedLetersInAll = lineArray[line-1].text!.count
         
         var tempCorrectLetterCount = 0
         tempCorrectLetterCount = 0
         
-        for i in 1...lineOne!.text!.count-1 {
-            let inputLetter = lineOne!.text![lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i)]
-            let origenalLetter = labelOne!.text![labelOne!.text!.index(labelOne!.text!.startIndex, offsetBy: i-1)]
+        for i in 1...lineArray[line-1].text!.count-1 {
+            let inputLetter = lineArray[line-1].text![lineArray[line-1].text!.index(lineArray[line-1].text!.startIndex, offsetBy: i)]
+            let origenalLetter = labelArray[line-1].text![labelArray[line-1].text!.index(labelArray[line-1].text!.startIndex, offsetBy: i-1)]
             
             if inputLetter == origenalLetter {
                 tempCorrectLetterCount += 1
             } else {
-                lineOne!.text!.remove(at: lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i))
-                lineOne!.text!.insert("_", at: lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i))
+                lineArray[line-1].text!.remove(at: lineArray[line-1].text!.index(lineArray[line-1].text!.startIndex, offsetBy: i))
+                lineArray[line-1].text!.insert("_", at: lineArray[line-1].text!.index(lineArray[line-1].text!.startIndex, offsetBy: i))
             }
         }
         
-        lineOneCorrectLetters = tempCorrectLetterCount
-        lineOneTappedLeters = lineOne!.text!.count - 1
+        correctLettersArray[line-1] = tempCorrectLetterCount
+        tappedLettersArray[line-1] = lineArray[line-1].text!.count - 1
         
-        correctLettersInAll = lineOneCorrectLetters
-        tappedLetersInAll = lineOneTappedLeters
+        correctLettersInAll = 0
+        for i in correctLettersArray {
+            correctLettersInAll += i
+        }
+        
+        tappedLetersInAll = 0
+        for i in tappedLettersArray {
+            tappedLetersInAll += i
+        }
         
         updateAccuracyLabel()
     }
     
     //User is editing lineOne
     @IBAction func lineOneChanged(_ sender: Any) {
-        if lineOne.text?.count == labelOne.text!.count+1 {
-            lineOne.resignFirstResponder()
-            lineTow.text = " "
-            lineTow.becomeFirstResponder()
-            
-            labelArray[0].textColor = .gray
-            labelArray[1].textColor = .darkGray
-        }
-        
-        guard lineOne.text != nil else { return }
-        
-        if lineOne.text!.count == 0 {
-            lineOne.text = " "
-        }
-        
-        guard labelOne.text != nil else { return }
-        guard lineOne.text!.count >= 2 else { return }
-        tappedLetersInAll = lineOne.text!.count
-        
-        var tempCorrectLetterCount = 0
-        tempCorrectLetterCount = 0
-        
-        for i in 1...lineOne!.text!.count-1 {
-            let inputLetter = lineOne!.text![lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i)]
-            let origenalLetter = labelOne!.text![labelOne!.text!.index(labelOne!.text!.startIndex, offsetBy: i-1)]
-            
-            if inputLetter == origenalLetter {
-                tempCorrectLetterCount += 1
-            } else {
-                lineOne!.text!.remove(at: lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i))
-                lineOne!.text!.insert("_", at: lineOne!.text!.index(lineOne!.text!.startIndex, offsetBy: i))
-            }
-        }
-        
-        lineOneCorrectLetters = tempCorrectLetterCount
-        lineOneTappedLeters = lineOne!.text!.count - 1
-        
-        correctLettersInAll = lineOneCorrectLetters
-        tappedLetersInAll = lineOneTappedLeters
-        
-        updateAccuracyLabel()
-        
+        updateValue(line: 1)
     }
     
     @IBAction func lineTwoChaged(_ sender: Any) {
-        if lineTow.text?.count == labelTwo.text!.count+1 {
-            lineTow.resignFirstResponder()
-            lineThree.text = " "
-            lineThree.becomeFirstResponder()
-        }
-        
-        if lineTow.text?.count == 0 {
-            lineTow.resignFirstResponder()
-            lineOne.becomeFirstResponder()
-        }
+        updateValue(line: 2)
     }
     
     @IBAction func lineThreeChanged(_ sender: Any) {
-        if lineThree.text?.count == labelThree.text!.count+1 {
-            lineThree.resignFirstResponder()
-            lineFour.text = " "
-            lineFour.becomeFirstResponder()
-        }
-        
-        if lineThree.text?.count == 0 {
-            lineThree.resignFirstResponder()
-            lineTow.becomeFirstResponder()
-        }
+        updateValue(line: 3)
+    }
+    
+    @IBAction func lineFourChanged(_ sender: Any) {
+        updateValue(line: 4)
+    }
+    
+    @IBAction func lineFiveChanged(_ sender: Any) {
+        updateValue(line: 5)
+    }
+    
+    @IBAction func lineSixChanged(_ sender: Any) {
+        updateValue(line: 6)
+    }
+    
+    @IBAction func lineSevenChanged(_ sender: Any) {
+        updateValue(line: 7)
+    }
+    
+    @IBAction func lineEightChanged(_ sender: Any) {
+        updateValue(line: 8)
+    }
+    
+    @IBAction func lineNineChanged(_ sender: Any) {
+        updateValue(line: 9)
+    }
+    
+    @IBAction func lineTenChanged(_ sender: Any) {
+        updateValue(line: 10)
+    }
+    
+    @IBAction func lineElevenChanged(_ sender: Any) {
+        updateValue(line: 11)
+    }
+    
+    @IBAction func lineTwelveChanged(_ sender: Any) {
+        updateValue(line: 12)
+    }
+    
+    @IBAction func lineThirteenChanged(_ sender: Any) {
+        updateValue(line: 13)
+    }
+    
+    @IBAction func lineFourteenChanged(_ sender: Any) {
+        updateValue(line:14)
+    }
+    
+    @IBAction func lineFifteenChanged(_ sender: Any) {
+        updateValue(line: 15)
     }
     
     
@@ -346,6 +358,11 @@ class MarryHadALittleLambController:UITableViewController {
     
     
     @IBAction func doneTapped(_ sender: Any) {
+        stopTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(true)
         stopTimer()
     }
     
