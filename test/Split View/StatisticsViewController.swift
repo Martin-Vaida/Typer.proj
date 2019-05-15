eba9532cc66597ddab0dbccf27aa8194b9c46e2a
Vaida <goodluckfanweida@163.com>
2019-05-15 10:03:32 +0800

    1.0.29 Version One

--- test/Split View/StatisticsViewController.swift
+++ test/Split View/StatisticsViewController.swift
@@ -19,7 +19,7 @@ class StatisticsViewController:UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
         let normalCellHeight = CGFloat(44)
-        let largeCellHeihgt = CGFloat(244)
+        let largeCellHeihgt = CGFloat(254)
         if indexPath.section == 0 {
             return isViewHiddenå[indexPath.row] ? normalCellHeight : largeCellHeihgt
         } else {
@@ -35,5 +35,50 @@ class StatisticsViewController:UITableViewController {
         tableView.beginUpdates()
         tableView.endUpdates()
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeAverageAccuracyChart()
+    }
+    
+    //Make Cahrts
+        //Average Accurcy Charts
+    @IBOutlet weak var averageAccuracyView: UIView!
+    
+    func makeAverageAccuracyChart() {
+        
+        // Y-Axis Name Label
+        let yAxisNameLabel = UILabel(frame: CGRect(x: 0, y: 10, width: 60, height: 30))
+        yAxisNameLabel.text = "Percentage"
+        yAxisNameLabel.textColor = .lightGray
+        yAxisNameLabel.font = .systemFont(ofSize: 12)
+        yAxisNameLabel.textAlignment = .center
+        yAxisNameLabel.adjustsFontSizeToFitWidth = true
+        averageAccuracyView.addSubview(yAxisNameLabel)
+        
+        // Y-Axis Design
+        let yAxisArray:NSArray = ["100", "80", "60", "40", "20", "0"]
+        for i:Int in 0 ..< yAxisArray.count {
+            // Y-Axis Value
+            let yAxisValue = UILabel(frame: CGRect(x: 0, y: 40+CGFloat(i)*30, width: 30, height: 30))
+            yAxisValue.text = yAxisArray[i] as? String
+            yAxisValue.font = .systemFont(ofSize: 11)
+            yAxisValue.textAlignment = .center
+            yAxisValue.adjustsFontSizeToFitWidth = true
+            yAxisValue.textColor = .lightGray
+            averageAccuracyView.addSubview(yAxisValue)
+            // Y-Axis Line
+            let yAxisLine = UIView(frame: CGRect(x: 30, y: 55+30*CGFloat(i) , width: 600, height: 1))
+            if i == yAxisArray.count-1 {
+                yAxisLine.backgroundColor = .black
+            } else {
+                yAxisLine.backgroundColor = .lightGray
+            }
+            averageAccuracyView.addSubview(yAxisLine)
+        }
+        
+    }
+    
     
 }
