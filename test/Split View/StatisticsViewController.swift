beeac669a9a99b78f82a4131f2fd32c57672cff4
Vaida <goodluckfanweida@163.com>
2019-05-17 19:31:21 +0800

    1.0.35

--- test/Split View/StatisticsViewController.swift
+++ test/Split View/StatisticsViewController.swift
@@ -68,7 +68,7 @@ class StatisticsViewController:UITableViewController {
         case 1:
             switch indexPath.row {
             case 0:
-                makeAverageAccuracyChart(from: .averageScore)
+                makeScoreChart()
             default:
                 break
             }
@@ -151,9 +151,16 @@ class StatisticsViewController:UITableViewController {
         scoreCollectionß.append(Score(100,100,"100",true, .maryHadALittleLamb))
         scoreCollectionß.append(Score(50,100,"100",false, .maryHadALittleLamb))
         scoreCollectionß.append(Score(100,100,"100",false, .maryHadALittleLamb))
+        scoreCollectionß.append(Score(100,100,"10",false, .maryHadALittleLamb))
         
         guard scoreCollectionß.count >= 2 else { return }
         scoreCollectionß.remove(at: 0)
+        
+        for i in 0...scoreCollectionß.count-1 {
+            if Double(scoreCollectionß[i].timePassed)! < 60.0 {
+                scoreCollectionß.remove(at: i)
+            }
+        }
     }
     
     //Make Cahrts
@@ -176,7 +183,7 @@ class StatisticsViewController:UITableViewController {
     }
     
     func makeAverageAccuracyChart(from: rowName) {
-        let viewArray = [averageAccuracyView, highestAccuracyView, lowestAccuracyView, averageScoreView]
+        let viewArray = [averageAccuracyView, highestAccuracyView, lowestAccuracyView]
         var viewInt = 0
         
         switch from {
@@ -195,7 +202,6 @@ class StatisticsViewController:UITableViewController {
         var tapped = 0
         var heighest = 0
         var lowest = 100
-        var score = 0
         
         if from == .averageAccuracyRate {
             for i in scoreCollectionß {
@@ -217,17 +223,6 @@ class StatisticsViewController:UITableViewController {
                     lowest = average
                 }
             }
-        } else if from == .averageScore {
-            var tempCorrect = 0
-            var temptapped = 0
-            var tempTime = 0
-            for i in scoreCollectionß {
-                guard Double(i.timePassed)! > 60 else { continue }
-                tempCorrect += i.correctLetters
-                temptapped += i.tappedLatters
-                tempTime += Int(Double(i.timePassed)!)
-            }
-            score = tempCorrect*100/temptapped/tempTime*60
         }
         
         
@@ -370,12 +365,131 @@ class StatisticsViewController:UITableViewController {
             pathƒ.move(to: CGPoint(x: 30, y: 45+150-lowest*3/2))
             pathƒ.addLine(to: CGPoint(x: 630, y: 45+150-lowest*3/2))
             layer.path = pathƒ.cgPath
-        } else if from == .averageScore {
+        }
+    }
+    
+    func makeScoreChart() {
+        var score = 0
+        
+        for i in scoreCollectionß {
+            let scoreß = i.correctLetters*6000/i.tappedLatters/Int(Double(i.timePassed)!)
+            score += scoreß
+        }
+        score /= scoreCollectionß.count
+        
+        // Y-Axis Name Labels
+        let yAxisNameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 30))
+        yAxisNameLabel.text = "Percentage"
+        yAxisNameLabel.textColor = .lightGray
+        yAxisNameLabel.font = .systemFont(ofSize: 12)
+        yAxisNameLabel.textAlignment = .center
+        yAxisNameLabel.adjustsFontSizeToFitWidth = true
+        averageScoreView.addSubview(yAxisNameLabel)
+        
+        // Y-Axis Design
+        let yAxisArray:NSArray = ["250", "200", "150", "100", "50", "0"]
+        for i:Int in 0 ..< yAxisArray.count {
+            // Y-Axis Value
+            let yAxisValue = UILabel(frame: CGRect(x: 0, y: 30+CGFloat(i)*30, width: 30, height: 30))
+            yAxisValue.text = yAxisArray[i] as? String
+            yAxisValue.font = .systemFont(ofSize: 11)
+            yAxisValue.textAlignment = .center
+            yAxisValue.adjustsFontSizeToFitWidth = true
+            yAxisValue.textColor = .lightGray
+           averageScoreView.addSubview(yAxisValue)
+            
+        }
+        
+        //Set X-Axis Layer
+        let xlayer = CAShapeLayer()
+        xlayer.fillColor = UIColor.clear.cgColor
+        xlayer.lineWidth = 1.0
+        xlayer.strokeColor = UIColor.black.cgColor
+        averageScoreView.layer.addSublayer(xlayer)
+        
+        let pathß = UIBezierPath()
+        pathß.move(to: CGPoint(x: 30, y: 195))
+        pathß.addLine(to: CGPoint(x: 630, y: 195))
+        xlayer.path = pathß.cgPath
+        
+        //Set Y-Axis Layer
+        let ylayer = CAShapeLayer()
+        ylayer.fillColor = UIColor.clear.cgColor
+        ylayer.lineWidth = 1.0
+        ylayer.strokeColor = UIColor.black.cgColor
+        averageScoreView.layer.addSublayer(ylayer)
+        
+        let pathå = UIBezierPath()
+        pathå.move(to: CGPoint(x: 30, y: 30))
+        pathå.addLine(to: CGPoint(x: 30, y: 195))
+        ylayer.path = pathå.cgPath
+        
+        // X-Axis Name Labels
+        let xAxisNameLabel = UILabel(frame: CGRect(x: 600, y: 200, width: 60, height: 30))
+        xAxisNameLabel.text = "Times"
+        xAxisNameLabel.textColor = .lightGray
+        xAxisNameLabel.font = .systemFont(ofSize: 12)
+        xAxisNameLabel.textAlignment = .center
+        xAxisNameLabel.adjustsFontSizeToFitWidth = true
+        averageScoreView.addSubview(xAxisNameLabel)
+        
+        // X-Axis Design
+        var xAxisArray:[String] = []
+        for i in 0...scoreCollectionß.count {
+            xAxisArray.append("\(i)")
+        }
+        
+        
+        for i in 0 ..< xAxisArray.count {
+            // X-Axis value
+            let xAxisValue = UILabel(frame: CGRect(x: CGFloat(600/xAxisArray.count*i), y: 200, width: 30, height: 30))
+            xAxisValue.text = xAxisArray[i]
+            xAxisValue.font = .systemFont(ofSize: 12)
+            xAxisValue.adjustsFontSizeToFitWidth = true
+            xAxisValue.textAlignment = .right
+            xAxisValue.textColor = .lightGray
+            averageScoreView.addSubview(xAxisValue)
+        }
+        
+        //Set Layer
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.clear.cgColor
+        layer.lineWidth = 1.0
+        layer.strokeColor = UIColor.blue.cgColor
+        averageScoreView.layer.addSublayer(layer)
+        
+        let path = UIBezierPath()
+        //Starting Point
+        var xArray:[Int] = []
+        var yArray:[Double] = []
+        
+        for i in 1 ... scoreCollectionß.count {
+            xArray.append(30+600/xAxisArray.count*i)
+        }
+        
+        for i in scoreCollectionß {
+            let score = i.correctLetters*6000/i.tappedLatters/Int(Double(i.timePassed)!)
+            yArray.append(45.0+150.0 - Double(score)*150.0/250.0)
+        }
+        
+        path.move(to: CGPoint(x: xArray[0] , y: Int(yArray[0])))
+        for i in 1..<scoreCollectionß.count {
+            path.addLine(to: CGPoint(x: CGFloat(xArray[i]), y: CGFloat(yArray[i])))
+        }
+        layer.path = path.cgPath
+        
+        //Connect the points
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = 5.0
+        layer.add(animation, forKey: "strokeEnd")
+        
+        
+        for _ in 0...0 {
             let layer = CAShapeLayer()
             layer.fillColor = UIColor.clear.cgColor
             layer.lineWidth = 0.5
             layer.strokeColor = UIColor.red.cgColor
-            viewArray[viewInt]!.layer.addSublayer(layer)
+            averageScoreView.layer.addSublayer(layer)
             
             let pathƒ = UIBezierPath()
             pathƒ.move(to: CGPoint(x: 30, y: 45+150-score*3/2))
