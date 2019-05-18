06a88101cf4bc973d649d44d4714c5d16db603ba
Vaida <goodluckfanweida@163.com>
2019-05-18 11:55:28 +0800

    1.0.38

---
+++ test/DeveloperControllers/storyboardPic.swift
@@ -0,0 +1,14 @@
+//
+//  storyboardPic.swift
+//  Typer.proj
+//
+//  Created by Vaida on 2019/5/18.
+//  Copyright © 2019 Martin_Vaida. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class storyboardPic:UIViewController {
+    
+}
---
+++ test/DeveloperControllers/storyboardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  storyboardViewController.swift
+//  Typer.proj
+//
+//  Created by Vaida on 2019/5/18.
+//  Copyright © 2019 Martin_Vaida. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class storyBoardViewController:UIViewController {
+    @IBAction func uniwndToStoryboardViewController(segue:UIStoryboardSegue) {
+        
+    }
+}
